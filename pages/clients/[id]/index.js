7689b5bf12b3038bdc875d5d863cc267d4178e5e
Jerry Wang <doghost111@gmail.com>
2023-07-19 01:22:54 +0800

    feat : link 的使用方法 - next 推薦的 參數引用方法

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,23 +1,22 @@
 import { useRouter } from "next/router";
 function ClientId() {
-    const router = useRouter();
-    console.log(router.query);
-    console.log(router.pathname);
-    const loadProjectHandler = () => {
-        // load data...
-        // 兩種 router 寫法 
-        // router.push("/clients/max/projectA");
-        router.push({
-            pathname: "/clients/[id]/[clientId]",
-            query: { id: "max", clientId: "projectA" },
-        });
-    };
-    return (
-        <div>
-            <h1>This is client {router.query.id} Page.</h1>
-            <button>Load Project A</button>
-        </div>
-    );
+  const router = useRouter();
+  console.log(router.query);
+  const loadProjectHandler = () => {
+    // load data...
+    // 兩種 router 寫法
+    // router.push("/clients/max/projectA");
+    router.push({
+      pathname: "/clients/[id]/[clientId]",
+      query: { id: "max", clientId: "projectA" },
+    });
+  };
+  return (
+    <div>
+      <h1>This is client {router.query.id} Page.</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
+    </div>
+  );
 }
 
 export default ClientId;
--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,32 +1,28 @@
 import Link from "next/link";
 
 function client() {
-    const clients = [
-        { id: "max", name: "Maximilian" },
-        { id: "manu", name: "Manuel" },
-    ];
-    return (
-        <div>
-            <h1>This is client Page.</h1>
-            <ul>
-                {clients.map((client) => (
-                    <li key={client.id}>
-                        <Link
-                            href={{
-                                pathname: "/clients/[id]",
-                                query: { id: client.id },
-                            }}
-                        >
-                            {/* <Link
-                            href={`/clients/${client.id}`}
-                        > */}
-                            {client.name}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  const clients = [
+    { id: "max", name: "Maximilian" },
+    { id: "manu", name: "Manuel" },
+  ];
+  return (
+    <div>
+      <h1>This is client Page.</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link
+              href={{
+                pathname: "/clients/[id]",
+                query: { id: client.id },
+              }}
+            >{client.name}</Link>
+            {/* <Link href={`/clients/${client.id}`}>{client.name}</Link> */}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default client;
